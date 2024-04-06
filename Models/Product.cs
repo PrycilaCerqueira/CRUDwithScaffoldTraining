033b7b1b0e381df4b8a9fba8e24f2358b496dd56
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2024-04-05 20:54:35 -0400

    Product class created

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.Transactions;
+
+namespace CRUDwithScaffoldTraining.Models
+{
+    public class Product
+    {
+        public int Prod_Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Maker { get; set; }
+        public string? SKU { get; set; }
+
+        public int? Qty_InStock { get; set; }
+        public double? UnitPrice { get; set; }
+        public double? UnitPrice_Total {  get; set; }
+        
+        public int?  Qty_Purchased { get; set; }
+        public double? CostPrice { get; set; }
+        public double? CostPrice_Total { get; set; }
+
+    }
+}
