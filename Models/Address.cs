36a8eabba5b62ba0e9b56fd74f939167efa73038
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2024-04-08 20:08:11 -0400

    Address and PurchaseOrder Models created

---
+++ Models/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUDwithScaffoldTraining.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string street { get; set; }
+        public int street_number { get; set; }
+        public string unit { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string country { get; set; }
+    }
+}
---
+++ Models/PurchaseOrder.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUDwithScaffoldTraining.Models
+{
+    public class PurchaseOrder
+    {
+        public int Id { get; set; }
+        public string Quote { get; set; }
+        public string Invoice { get; set; }
+        public string Supplier { get; set; }
+        public List<Address> Supplier_Address {  get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string SalesRep_Name { get; set; }
+    }
+}
